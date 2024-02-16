de92b6f540e69db50105e688318f2248be47f232
Tapas Jyoti <tapasjyoti8327@gmail.com>
2024-02-16 13:33:53 +0530

    first commit

---
+++ Scripts/bubble_sort.js
@@ -0,0 +1,13 @@
+function Bubble() {
+    //Setting Time complexities
+    document.getElementById("Time_Worst").innerText = "O(N^2)";
+    document.getElementById("Time_Average").innerText = "Θ(N^2)";
+    document.getElementById("Time_Best").innerText = "Ω(N)";
+
+    //Setting Space complexity
+    document.getElementById("Space_Worst").innerText = "O(1)";
+
+    c_delay = 0;
+
+    
+}
